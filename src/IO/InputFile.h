d7816424f28a71865fed0e0103cf5eed5e04b579
Andrea Zoli <zoli@iasfbo.inaf.it>
2013-07-25 16:26:47 +0200

    Removed GetNextPeriod, GetTime functions.

--- src/IO/InputFile.h
+++ src/IO/InputFile.h
@@ -46,13 +46,6 @@ class InputFile : public File {
 
 		virtual int64_t GetNEvents();
 
-/* FIXME this shouldn't stay here..
-		virtual int64_t GetNextRowPeriod(uint32_t timeColumnNumber, int64_t pos_first, double end_time) = 0;
-
-		virtual double* GetTime(uint32_t timeColumnNumber, uint64_t start, uint64_t dim) = 0;
-
-		virtual double GetTime(uint32_t timeColumnNumber, uint64_t pos) = 0;*/
-
 		virtual int32_t GetStatus() { return status; };
 
 		virtual uint32_t GetIndexFirstColumn() = 0;
