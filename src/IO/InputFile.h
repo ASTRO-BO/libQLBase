f4d2e2524afa48aaec10a570a1790099d1c84e74
Andrea Zoli <zoli@iasfbo.inaf.it>
2013-08-23 15:04:56 +0200

    Changed license header format.

--- src/IO/InputFile.h
+++ src/IO/InputFile.h
@@ -1,21 +1,17 @@
-/*
- * Copyright (C) 2001 Andrea Bulgarelli
- *               2013 Andrea Zoli
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+/***************************************************************************
+    begin                : Jul 18 2013
+    copyright            : (C) 2013 Andrea Zoli
+    email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef QL_IO_INPUTFILE_H
 #define QL_IO_INPUTFILE_H
