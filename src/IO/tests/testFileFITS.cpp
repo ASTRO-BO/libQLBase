68328b603aa623fcad6af695160806d08a3009e4
Andrea Zoli <zoli@iasfbo.inaf.it>
2013-08-23 15:10:23 +0200

    Added the license header for testFileFITS.cpp

--- src/IO/tests/testFileFITS.cpp
+++ src/IO/tests/testFileFITS.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+    begin                : Jul 29 2013
+    copyright            : (C) 2013 Andrea Zoli
+    email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
+
 #include<IO/InputFileFITS.h>
 #include<IO/OutputFileFITS.h>
 #include<sstream>
