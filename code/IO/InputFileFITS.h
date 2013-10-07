3619cab021a17680a7d568b26fd7e53e655311f8
Andrea Zoli <zoli@iasfbo.inaf.it>
2013-10-07 10:25:44 +0200

    Added doxygen comments to InputFileFITS functions.

--- code/IO/InputFileFITS.h
+++ code/IO/InputFileFITS.h
@@ -22,6 +22,7 @@
 
 namespace qlbase {
 
+/// FITS file reader (cfitsio wrapping class).
 class InputFileFITS : public InputFile {
 
 	public:
@@ -30,34 +31,85 @@ class InputFileFITS : public InputFile {
 
 		virtual ~InputFileFITS();
 
+		/// Open a fits file.
 		virtual void open(const std::string &filename);
+
+		/// Close a fits file.
 		virtual void close();
 		virtual bool isOpened() { return opened; }
 
+		/// Point to a specific header.
+		/// /param[in] number Number of the header.
 		virtual void moveToHeader(int number);
 
+		/// Get the number of columns.
 		virtual int getNCols();
+
+		/// Get the number of rows.
 		virtual long getNRows();
 
+		/// Read a part of a column of bytes (fits type 1B).
+		/// \param[in] ncol Column number.
+		/// \param[in] frow First row.
+		/// \param[in] lrow Last row.
+		/// \return The vector of bytes.
 		virtual std::vector<uint8_t> readu8i(int ncol, long frow, long lrow);
+
+		/// Read a part of a column of bytes (fits type 1I).
 		virtual std::vector<int16_t> read16i(int ncol, long frow, long lrow);
+
+		/// Read a part of a column of bytes (fits type 1J).
 		virtual std::vector<int32_t> read32i(int ncol, long frow, long lrow);
+
+		/// Read a part of a column of bytes (fits type 1K).
 		virtual std::vector<int64_t> read64i(int ncol, long frow, long lrow);
+
+		/// Read a part of a column of bytes (fits type 1E).
 		virtual std::vector<float> read32f(int ncol, long frow, long lrow);
+
+		/// Read a part of a column of bytes (fits type 1D).
 		virtual std::vector<double> read64f(int ncol, long frow, long lrow);
 
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20B).
+		/// \param[in] ncol Column number.
+		/// \param[in] frow First row.
+		/// \param[in] lrow Last row.
+		/// \param[in] vsize Size of the the vector (this represent the size of a cell).
+		/// \return The column of vector of bytes (vector of vector of bytes).
 		virtual std::vector< std::vector<uint8_t> > readu8iv(int ncol, long frow, long lrow, int vsize);
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20I).
 		virtual std::vector< std::vector<int16_t> > read16iv(int ncol, long frow, long lrow, int vsize);
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20J).
 		virtual std::vector< std::vector<int32_t> > read32iv(int ncol, long frow, long lrow, int vsize);
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20K).
 		virtual std::vector< std::vector<int64_t> > read64iv(int ncol, long frow, long lrow, int vsize);
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20E).
 		virtual std::vector< std::vector<float> > read32fv(int ncol, long frow, long lrow, int vsize);
+
+		/// Read a part of a column of vector of bytes (fits type for es. 20D).
 		virtual std::vector< std::vector<double> > read64fv(int ncol, long frow, long lrow, int vsize);
 
+		/// Read a multidimensional image of bytes.
 		virtual Image<uint8_t> readImageu8i();
+
+		/// Read a multidimensional image of 16bit integers.
 		virtual Image<int16_t> readImage16i();
+
+		/// Read a multidimensional image of 32bit integers.
 		virtual Image<int32_t> readImage32if();
+
+		/// Read a multidimensional image of 64bit integers.
 		virtual Image<int64_t> readImage64i();
+
+		/// Read a multidimensional image of 32bit floats.
 		virtual Image<float> readImage32f();
+
+		/// Read a multidimensional image of 64bit floats.
 		virtual Image<double> readImage64f();
 
 	private:
